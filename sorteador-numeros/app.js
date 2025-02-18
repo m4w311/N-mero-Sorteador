d402489187a10dafc131bd44225fbbe828c1440e
Maxwell Amorim <maxwellnotrabalho@gmail.com>
2025-02-18 12:05:01 -0300

    functionReturn

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,9 +1,10 @@
 function sortear(){
-    let quantidade = document.getElementById("quantidade").value;
-    let de = document.getElementById("de").value;
-    let ate = document.getElementById("ate").value;
-
-    alert(`Quantidade: ${quantidade}`);
-    alert(`Do número:${de}`);
-    alert(`Até o número: ${ate}`);
+    let quantidade = parseInt(document.getElementById("quantidade").value);
+    let de = parseInt(document.getElementById("de").value);
+    let ate = parseInt(document.getElementById("ate").value);
+    let numero = obterNumeroAleatorio (de, ate);
+    alert(numero);
 } 
+function obterNumeroAleatorio(min, max){
+    return Math.floor(Math.random() *(max - min + 1)) + min ;
+}
